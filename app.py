506a745778cd937288d0468f622e8040e4f658e0
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-06-26 09:06:01 -0400

    Removed unneccessary DB_USE_TLS environment var addition

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ encryption_key = bytes.fromhex(HEX_ENCRYPTION_KEY)
 # Only bypass SSL verification in development
 client = AsyncIOMotorClient(
     Config.DB_CONNECTION_STRING,
-    tls=(Config.DB_USE_TLS == "true"),
+    tls=(Config.ENV != 'development'),
     tlsAllowInvalidCertificates=(Config.ENV == 'development'),
     connectTimeoutMS=30000,
     serverSelectionTimeoutMS=30000
