fa34eb477e21ac42bf19bdeb769928d1c4757bc3
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-07-16 17:20:51 -0400

    Added function implementations for mongodb module.

--- mongodb.py
+++ mongodb.py
@@ -4,19 +4,38 @@ from motor.motor_asyncio import (
     AsyncIOMotorCollection,
 )
 
+
 from config import Config
 
 _client: AsyncIOMotorClient
 _db: AsyncIOMotorDatabase
 
-def initialize_db() -> bool:
-    pass
+def initialize_db() -> None | Exception:
+    try:
+        global _client, _db
+        _client = AsyncIOMotorClient(
+            Config.DB_CONNECTION_STRING,
+            tls=(Config.ENV != 'development'),
+            tlsAllowInvalidCertificates=(Config.ENV == 'development'),
+            connectTimeoutMS=30000,
+            serverSelectionTimeoutMS=30000
+        )
+
+        _db = _client[Config.DATABASE]
+
+    except Exception as err:
+        return err
 
 def get_client() -> AsyncIOMotorClient:
-    pass
+    return _client
 
 def get_db() -> AsyncIOMotorDatabase:
-    pass
+    return _db
 
-def close_connection() -> bool:
-    pass
+def close_connection() -> None | Exception:
+    try:
+        _client.close()
+    except Exception as err:
+        return err
+
+initialize_db()
