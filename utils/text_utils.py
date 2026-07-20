11954e7515b5d0f32ef38eddd57c49d028bbd353
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-07-19 22:07:08 -0400

    Added hash_text function.

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -1,6 +1,6 @@
 import re
 from bs4 import BeautifulSoup
-
+import hashlib
 
 def normalize_text(string: str) -> str:
     if not string:
@@ -9,3 +9,8 @@ def normalize_text(string: str) -> str:
     string = BeautifulSoup(string, "html.parser").get_text(separator=' ')
     string = re.sub(r"\s+", ' ', string).strip().lower()
     return string
+
+def hash_text(string: str) -> str:
+    text_hash = hashlib.new("sha256")
+    text_hash.update(string.encode("utf-8"))
+    return text_hash.hexdigest()
