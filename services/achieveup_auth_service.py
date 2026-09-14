c33bd438847f17e754133e1430d2fa921b105805
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-09-14 11:38:09 -0400

    Function to search canvas schools finished.

--- services/achieveup_auth_service.py
+++ services/achieveup_auth_service.py
@@ -546,46 +546,54 @@ def require_instructor_role(func):
     
     return wrapper 
 
-async def search_canvas_schools(email: str, url: str = "https://canvas.instructure.com/api/v1/accounts/search?") -> list[str]:
+async def search_canvas_schools(school_name: str, url: str = "https://canvas.instructure.com/api/v1/accounts/search?") -> dict:
 
     from services.achieveup_canvas_service import create_canvas_session
     from services.canvas_submissions_service import check_rate_limit
 
-    email_elements: tuple = tuple(email.rsplit('@', 1)[1].split('.')[-2:])
-
-
-    # # params: dict = {"domain": "ucf",}
-    # params: dict = {}
-
+    # Define request parameters.
     params = {
         'per_page': 100,
-        "name": "University of cent"
+        "name": school_name
     }
 
     schools: list = []
 
     try:
-        # Check using edu email domain.
         async with create_canvas_session() as session:
             parse_pages: bool = True
             while parse_pages:
                 async with session.get(url, params=params) as res:
+
+                    if res.status != 200:
+                        res_error: str = await res.text()
+
+                        logger.error(f"Error fetching Canvas school records: {res.status} - {res_error}")
+                        return {
+                            'error': f'Failed to fetch schools: {res.status}',
+                            'statusCode': res.status
+                        }
+
                     school_data: dict = await res.json()
                     schools.extend([x.get("name", "") for x in school_data])
 
                     await check_rate_limit(res)
 
+                    # Check headers to see if response returned more than one page.
                     url_headers: str = res.headers.get("Link", "")
                     if "rel=\"next\"" in url_headers:
                         for header_link in url_headers.split(","):
                             if 'rel="next"' in header_link:
+
+                                # Set new request values.
                                 url = header_link.split(';')[0].strip('<> ')
+                                params = None
+                                break
                     else:
                         parse_pages = False
+
     except Exception as error:
-        pass
-        # async with session.get()
-
-    # Perform WHOIS lookup.
-
-    return schools
+        logger.error(f"Error searching for schools utilizing Canvas: {str(error)}")
+        return {"error": "Internal server error.", "statusCode": 500}
+    
+    return {"schools": schools}
